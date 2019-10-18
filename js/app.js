d3cd9a3bff371b2455fd7d813bc7cc565bddce41
Ayesha Munawar <ayeshamunawarofficial@gmail.com>
2019-10-18 14:00:59 +0500

    Pig-Game> add basic HTML and CSS to the game and create a random dice value from 1 to 6 and set the current round score of the active player to that value

---
+++ js/app.js
@@ -0,0 +1,53 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+var scores, roundScore, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random()*6)+1;
+console.log("dice value : "+ dice);
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-'+ activePlayer).innerHTML= '<strong>'+ dice+'</strong>'
+
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
