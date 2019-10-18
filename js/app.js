dfae3af9a42d9f438d41404077b9fea0b34384d6
Ayesha Munawar <ayeshamunawarofficial@gmail.com>
2019-10-18 16:25:55 +0500

    Pig-Game> 1) Add functionality of changing the active player when dice rolls one and reset the round scores of both players 2) if dice rolls other than one add to round score and display it 3) hide the dice when player rolls one and show it again for next player

--- js/app.js
+++ js/app.js
@@ -8,46 +8,49 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
-scores = [0,0];
+scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random()*6)+1;
-console.log("dice value : "+ dice);
+//hide the dice in the begining of the game
+document.querySelector(".dice").style.display = "none";
+//set the global scores of both players to 0 at first
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//Roll the the dice and if the value if the rolled dice is one reset the score in round  and set active player as the other player
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  //1. Get a randome number for dice value between 1 and 6
+  var dice = Math.floor(Math.random() * 6) + 1;
+  console.log("dice value : " + dice);
+  //2. Display the result
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "../images/dice-" + dice + ".png";
+  //3. Update round score if the rolled number is not one
+  if (dice !== 1) {
+    //add score
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    //set nextplayer after resetting round score
+    roundScore = 0;
+    document.getElementById("current-0").textContent = roundScore;
+    document.getElementById("current-1").textContent = roundScore;
+    // document.querySelector(".player-0-panel").classList.remove("active");
+    // document.querySelector(".player-1-panel").classList.add("active");
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    diceDOM.style.display = "none";
+  }
+});
+
+// var x = document.querySelector("#score-0").textContent;
+// console.log(x);
+//  document.querySelector("#current-" + activePlayer).textContent = dice;
 //document.querySelector('#current-'+ activePlayer).innerHTML= '<strong>'+ dice+'</strong>'
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
-
-document.querySelector('.dice').style.display = 'none';
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
