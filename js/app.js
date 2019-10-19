913788cd2da7e0b7c404478753112af0b1b7f599
Ayesha Munawar <ayeshamunawarofficial@gmail.com>
2019-10-19 13:58:20 +0500

    Pig-Game-4-two-dice>  1) Add two dice in the game instead of one dice 2) if any one of dice rolls one next player gets th turn 3) if any one dice on a round for a player shows two consecutive 6s the player looses the entire game 4) show the winning limir form again after hitting new game

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
-Extra rules:
+EXTRA RULES:
 -A player looses a game when he rolls two sixes in a row all score global present previous 
 -input field to set limit from html
 -second dice player looses current score if only one of them is a one 
@@ -17,6 +17,8 @@ var scores,
   roundScore,
   previousRoll,
   currentRoll,
+  previousRoll2,
+  currentRoll2,
   activePlayer,
   winningLimit,
   isGamePlaying;
@@ -34,6 +36,7 @@ function init() {
   toggleErrorMessage('',0);
   resetPreviousCurrentRoundScore();
   document.querySelector(".dice").style.display = "none";
+  document.querySelector(".dice2").style.display = "none";
   document.getElementById("score-0").textContent = 0;
   document.getElementById("score-1").textContent = 0;
   document.getElementById("current-0").textContent = 0;
@@ -58,32 +61,43 @@ document.getElementById("btn-limit").addEventListener("click", function() {
  
 }, false);
 
-//Roll the the dice and if the value if the rolled dice is one reset the score in round  and set active player as the other player
+//Roll the the two dices and if the value if atleast one rolled dice is one reset the score in round  and set active player as the other player
 document.querySelector(".btn-roll").addEventListener("click", function() {
   if (isGamePlaying) {
     //1. Get a randome number for dice value between 1 and 6
     var dice = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
     //2. Display the result
     var diceDOM = document.querySelector(".dice");
+    var diceDOM2 = document.querySelector(".dice2");
     diceDOM.style.display = "block";
+    diceDOM2.style.display = "block";
     diceDOM.src = "../images/dice-" + dice + ".png";
+    diceDOM2.src = "../images/dice-" + dice2 + ".png";
     //3. Update round score if the rolled number is not one
-    if (dice !== 1) {
-      if (dice === 6 && currentRoll === 6) {
+    if (dice !== 1 && dice2 !== 1) {
+      if (
+        (dice === 6 && currentRoll === 6) ||
+        (dice2 === 6 && currentRoll2 === 6)
+      ) {
         let message =
-          "Oops ! two consecutive sixes player no." +
+          "Oops ! two consecutive sixes for player no." +
           (activePlayer + 1) +
           " looses";
         toggleErrorMessage(message, 1);
         previousRoll = currentRoll;
+        previousRoll2= currentRoll2;
         currentRoll = dice;
+        currentRoll2 = dice2;
         printCurrentAndPreviousDiceValuesForActivePlayer();
         makeActivePlayerLoose();
       } else {
         previousRoll = currentRoll;
+        previousRoll2 = currentRoll2;
         currentRoll = dice;
-        //add score to round score
-        roundScore += dice;
+        currentRoll2 = dice2;
+        //add dice values for both dices into round score
+        roundScore += (dice + dice2);
         //update Ui score of active player to round score as current score
         document.querySelector(
           "#current-" + activePlayer
@@ -116,6 +130,7 @@ function makeActivePlayerLoose() {
     .querySelector(".player-" + activePlayer + "-panel")
     .classList.remove("active");
   document.querySelector(".dice").style.display = "none";
+  document.querySelector(".dice2").style.display = "none";
 }
 
 /* 
@@ -140,6 +155,7 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
         .querySelector(".player-" + activePlayer + "-panel")
         .classList.remove("active");
       document.querySelector(".dice").style.display = "none";
+      document.querySelector(".dice2").style.display = "none";
     } else {
       //4. Change the active player
       nextPlayer();
@@ -152,6 +168,7 @@ set round score of both players to 0 and toggle activeplayer hide the dice
 */
 function nextPlayer() {
   var diceDOM = document.querySelector(".dice");
+  var diceDOM2 = document.querySelector(".dice2");
   resetPreviousCurrentRoundScore();
   document.getElementById("current-0").textContent = roundScore;
   document.getElementById("current-1").textContent = roundScore;
@@ -159,18 +176,20 @@ function nextPlayer() {
   document.querySelector(".player-1-panel").classList.toggle("active");
   activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
   diceDOM.style.display = "none";
+  diceDOM2.style.display = "none";
 }
 
 function printCurrentAndPreviousDiceValuesForActivePlayer() {
   if (currentRoll === 6 && previousRoll === 6) {
     console.log(
-      "Active user failed as current and previous roll is 6   : " + currentRoll,
-      previousRoll
+      "Active user failed as current and previous roll is 6   : " + currentRoll,currentRoll2,
+      previousRoll, previousRoll2
     );
+  
   } else {
     console.log(
-      "dice value for player " + (activePlayer + 1) + " is  :   " + currentRoll,
-      previousRoll
+      "dice value for player " + (activePlayer + 1) + " is  :   " + currentRoll, currentRoll2,
+      previousRoll,previousRoll2
     );
   }
 }
@@ -188,9 +207,12 @@ function resetPreviousCurrentRoundScore() {
   previousRoll = 0;
   currentRoll = 0;
   roundScore = 0;
+  currentRoll2 = 0;
+  previousRoll2 = 0;
 }
 
 document.querySelector(".btn-new").addEventListener("click", function() {
+   document.querySelector(".limit-form").classList.remove("hide");
   isGamePlaying=false;
   init();
 });
